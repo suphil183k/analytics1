c1ca0c51af35465f69307656a01ed5742a9cab26
suphil183k <suphil18@gmail.com>
2018-08-19 19:51:50 +0530

    now

--- file1.R
+++ file1.R
@@ -2,3 +2,14 @@ mtcars
 git config --global user.email "suphil18@gmail.com"
 git config --global user.name "suphil183k"
 women
+plot(women)
+install.packages('car')
+?women
+?mean
+x = 1:100
+y
+x
+y = seq(1,100,by=2)
+y
+y = seq(1,100,by=2)
+x = 1:100
